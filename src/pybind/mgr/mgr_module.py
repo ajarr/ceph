fed57407319f2051286a04de9f21e92936ee8737
Ramana Raja <rraja@redhat.com>
2023-01-18 20:04:00 -0500

    mgr/mgr_module: remove MgrStandbyModule's _cleanup() method
    ... that is not used.
    
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- src/pybind/mgr/mgr_module.py
+++ src/pybind/mgr/mgr_module.py
@@ -803,11 +803,7 @@ class MgrStandbyModule(ceph_module.BaseMgrStandbyModule, MgrModuleLoggingMixin):
         self._logger = self.getLogger()
 
     def __del__(self) -> None:
-        self._cleanup()
         self._unconfigure_logging()
-
-    def _cleanup(self) -> None:
-        pass
 
     @classmethod
     def _register_options(cls, module_name: str) -> None:

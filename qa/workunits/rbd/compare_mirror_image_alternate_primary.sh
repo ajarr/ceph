1d33fadb04e6b324b918e0d5c23d31d9190be07d
Christopher Hoffman <choffman@redhat.com>
2023-05-25 16:48:12 +0000

    qa: Add tests to validate synced images on rbd-mirror
    Introduce integration tests to validate the checksums of images synced
    using snapshot based rbd mirroring.
    
    Run workload on an image using a krbd or nbd client. Take mirror
    snapshots of the image under workload. Unmap and demote the primary
    image. Calculate the MD5 checksum of the image's most recent mirror
    snapshot. Wait for the syncing of the mirror snapshot to complete.
    Promote the non-primary image in the other cluster. Calculate the
    MD5 checksum of the newly promoted image. Check whether the hashes of
    the demoted image's and promoted image's latest mirror snapshots are
    the same.
    
    The above test is run as part of two different workunits:
     - a workunit that validates the syncing of multiple images with
       workloads running on them
     - another workunit that validates the syncing of a single image with
       workload running on it and the primary image alternates between
       the clusters.
    
    Fixes: https://tracker.ceph.com/issues/61617
    Signed-off-by: Christopher Hoffman <choffman@redhat.com>
    Co-authored-by: Ramana Raja <rraja@redhat.com>

---
+++ qa/workunits/rbd/compare_mirror_image_alternate_primary.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -ex
+
+CEPH_ARGS=''
+IMAGE=image-alternate-primary
+MIRROR_IMAGE_MODE=snapshot
+MIRROR_POOL_MODE=image
+MOUNT=test-alternate-primary
+RBD_IMAGE_FEATURES='layering,exclusive-lock,object-map,fast-diff'
+RBD_MIRROR_INSTANCES=1
+RBD_MIRROR_MODE=snapshot
+RBD_MIRROR_USE_EXISTING_CLUSTER=1
+WORKLOAD_TIMEOUT=5m
+
+. $(dirname $0)/rbd_mirror_helpers.sh
+
+take_mirror_snapshots() {
+  local cluster=$1
+  local pool=$2
+  local image=$3
+
+  for i in {1..30}; do
+    mirror_image_snapshot $cluster $pool $image
+    sleep 3s;
+  done
+}
+
+slow_untar_workload() {
+  local tarball_src_path=$1
+  local mountpt=$2
+  local timeout=$3
+
+  cp $tarball_src_path $mountpt/kernel.tar.gz
+  # run workload that updates the data and metadata of multiple files on disk.
+  # rate limit the workload such that the mirror snapshots can be taken as the
+  # contents of the image are progressively changed by the workload.
+  timeout $timeout bash -c "zcat $mountpt/kernel.tar.gz \
+    | pv -L 256K | tar xf - -C $mountpt" || true
+}
+
+setup
+
+start_mirrors ${CLUSTER1}
+start_mirrors ${CLUSTER2}
+
+# initial setup
+create_image_and_enable_mirror ${CLUSTER1} ${POOL} ${IMAGE} \
+  ${RBD_MIRROR_MODE} 10G
+
+if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+  DEV=$(sudo rbd --cluster ${CLUSTER1} device map -t nbd \
+           -o try-netlink ${POOL}/${IMAGE})
+elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+  DEV=$(sudo rbd --cluster ${CLUSTER1} device map -t krbd \
+           ${POOL}/${IMAGE})
+else
+  echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
+  return 1
+fi
+sudo mkfs.ext4 ${DEV}
+mkdir ${MOUNT}
+
+TARBALL_SRC=kernel.tar.gz
+wget https://download.ceph.com/qa/linux-5.4.tar.gz -O ${TARBALL_SRC}
+
+for i in {1..25}; do
+  # create mirror snapshots every few seconds under I/O
+  sudo mount ${DEV} ${MOUNT}
+  sudo chown $(whoami) ${MOUNT}
+  take_mirror_snapshots ${CLUSTER1} ${POOL} ${IMAGE} &
+  slow_untar_workload ${TARBALL_SRC} ${MOUNT} ${WORKLOAD_TIMEOUT}
+  wait
+
+  sudo umount ${MOUNT}
+
+  # calculate hash before demotion of primary image
+  DEMOTE_MD5=$(sudo md5sum ${DEV} | awk '{print $1}')
+  sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} ${DEV}
+
+  demote_image ${CLUSTER1} ${POOL} ${IMAGE}
+  wait_for_status_in_pool_dir ${CLUSTER1} ${POOL} ${IMAGE} 'up+unknown'
+  wait_for_status_in_pool_dir ${CLUSTER2} ${POOL} ${IMAGE} 'up+unknown'
+  promote_image ${CLUSTER2} ${POOL} ${IMAGE}
+
+  # calculate hash after promotion of secondary image
+  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+    DEV=$(sudo rbd --cluster ${CLUSTER2} device map -t nbd \
+             -o try-netlink ${POOL}/${IMAGE})
+  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+    DEV=$(sudo rbd --cluster ${CLUSTER2} device map -t krbd ${POOL}/${IMAGE})
+  fi
+  PROMOTE_MD5=$(sudo md5sum ${DEV} | awk '{print $1}')
+
+  if [[ "${DEMOTE_MD5}" != "${PROMOTE_MD5}" ]]; then
+    echo "Mismatch at iteration ${i}: ${DEMOTE_MD5} != ${PROMOTE_MD5}"
+    exit 1
+  fi
+
+  TEMP=${CLUSTER1}
+  CLUSTER1=${CLUSTER2}
+  CLUSTER2=${TEMP}
+done
+
+echo OK
---
+++ qa/workunits/rbd/compare_mirror_images.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+set -ex
+
+CEPH_ARGS=''
+IMG_PREFIX=image-primary
+MIRROR_IMAGE_MODE=snapshot
+MIRROR_POOL_MODE=image
+MNTPT_PREFIX=test-primary
+RBD_IMAGE_FEATURES='layering,exclusive-lock,object-map,fast-diff'
+RBD_MIRROR_INSTANCES=1
+RBD_MIRROR_MODE=snapshot
+RBD_MIRROR_USE_EXISTING_CLUSTER=1
+WORKLOAD_TIMEOUT=5m
+
+. $(dirname $0)/rbd_mirror_helpers.sh
+
+take_mirror_snapshots() {
+  local cluster=$1
+  local pool=$2
+  local image=$3
+
+  for i in {1..30}; do
+    mirror_image_snapshot $cluster $pool $image
+    sleep 3s;
+  done
+}
+
+slow_untar_workload() {
+  local tarball_src_path=$1
+  local mountpt=$2
+  local timeout=$3
+
+  cp $tarball_src_path $mountpt/kernel.tar.gz
+  # run workload that updates the data and metadata of multiple files on disk.
+  # rate limit the workload such that the mirror snapshots can be taken as the
+  # contents of the image are progressively changed by the workload.
+  timeout $timeout bash -c "zcat $mountpt/kernel.tar.gz \
+    | pv -L 256K | tar xf - -C $mountpt" || true
+}
+
+wait_for_image_removal () {
+  local cluster=$1
+  local pool=$2
+  local image=$3
+
+  for s in 1 2 4 8 8 8 8 8 8 8 8 16 16; do
+    if [[ -z $(rbd --cluster $cluster ls $pool | grep -w $image) ]]; then
+      echo "image:${image} removed from cluster:${cluster} pool:${pool}"
+      return 0
+    fi
+
+    echo "waiting for image ${image} to be removed from \
+      cluster:${cluster} pool:${pool} ..."
+    sleep $s
+  done
+
+  echo "image:${image} not removed from cluster:${cluster} pool:${pool}"
+  return 1
+}
+
+compare_demoted_promoted_mirror_snaps() {
+  local dev=$1
+  local img=${IMG_PREFIX}$2
+  local mntpt=${MNTPT_PREFIX}$2
+  local demote_md5 promote_md5
+
+  sudo umount ${mntpt}
+
+  # calculate hash before demotion of primary image
+  demote_md5=$(sudo md5sum ${dev} | awk '{print $1}')
+  sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} \
+      ${POOL}/${img}
+
+  demote_image ${CLUSTER1} ${POOL} ${img}
+  wait_for_status_in_pool_dir ${CLUSTER1} ${POOL} ${img} 'up+unknown'
+  wait_for_status_in_pool_dir ${CLUSTER2} ${POOL} ${img} 'up+unknown'
+  promote_image ${CLUSTER2} ${POOL} ${img}
+
+  # calculate hash after promotion of secondary image
+  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+    dev=$(sudo rbd --cluster ${CLUSTER2} device map -t nbd \
+             -o try-netlink ${POOL}/${img})
+  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+    dev=$(sudo rbd --cluster ${CLUSTER2} device map -t krbd ${POOL}/${img})
+  fi
+  promote_md5=$(sudo md5sum ${dev} | awk '{print $1}')
+  sudo rbd --cluster ${CLUSTER2} device unmap -t ${RBD_DEVICE_TYPE} ${dev}
+
+  if [[ "${demote_md5}" != "${promote_md5}" ]]; then
+    echo "Mismatch for ${POOL}/${img}: ${demote_md5} != ${promote_md5}"
+    return 1
+  fi
+}
+
+setup
+
+start_mirrors ${CLUSTER1}
+start_mirrors ${CLUSTER2}
+
+TARBALL_SRC=kernel.tar.gz
+wget https://download.ceph.com/qa/linux-5.4.tar.gz -O ${TARBALL_SRC}
+
+for i in {1..10}; do
+  DEVS=()
+  for j in {1..10}; do
+    IMG=${IMG_PREFIX}${j}
+    MNTPT=${MNTPT_PREFIX}${j}
+    create_image_and_enable_mirror ${CLUSTER1} ${POOL} ${IMG} \
+      ${RBD_MIRROR_MODE} 10G
+    if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+      DEV=$(sudo rbd --cluster ${CLUSTER1} device map -t nbd \
+	      -o try-netlink ${POOL}/${IMG})
+    elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+      DEV=$(sudo rbd --cluster ${CLUSTER1} device map -t krbd \
+	      ${POOL}/${IMG})
+    else
+      echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
+      return 1
+    fi
+    DEVS+=($DEV)
+    sudo mkfs.ext4 ${DEV}
+    mkdir ${MNTPT}
+    sudo mount ${DEV} ${MNTPT}
+    sudo chown $(whoami) ${MNTPT}
+    # create mirror snapshots under I/O every few seconds
+    take_mirror_snapshots ${CLUSTER1} ${POOL} ${IMG} &
+    slow_untar_workload ${TARBALL_SRC} ${MNTPT} ${WORKLOAD_TIMEOUT} &
+  done
+  wait
+
+  for j in {1..10}; do
+    compare_demoted_promoted_mirror_snaps ${DEVS[$j-1]} $j &
+  done
+  wait
+
+  for j in {1..10}; do
+    IMG=${IMG_PREFIX}${j}
+    # Allow for removal of non-primary image by checking that mirroring
+    # image status is "up+replaying"
+    wait_for_replaying_status_in_pool_dir ${CLUSTER1} ${POOL} ${IMG}
+    remove_image ${CLUSTER2} ${POOL} ${IMG}
+    wait_for_image_removal ${CLUSTER1} ${POOL} ${IMG}
+    rm -rf ${MNTPT_PREFIX}${j}
+  done
+done
+
+echo OK

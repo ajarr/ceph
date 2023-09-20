3330d2593b36c3bc77d986afc12c20a3c8ad2f3b
Ramana Raja <rraja@redhat.com>
2023-09-20 12:15:06 -0400

    rbd_support_module_recovery_dd_workload: dd workload instead of fio
    Signed-off-by: Ramana Raja <rraja@redhat.com>

---
+++ qa/workunits/rbd/rbd_support_module_recovery_dd_workload.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -ex
+
+POOL=rbd
+IMAGE_PREFIX=image
+MNT_DIR_PREFIX=mnt
+NUM_IMAGES=20
+RUN_TIME=3600
+
+per_image_workload() {
+  local DEV=$1
+  local CURRENT_TIME=$(date +%s)
+  local END_TIME=$((${CURRENT_TIME}+${RUN_TIME}))
+  while [[ ${CURRENT_TIME} -le ${END_TIME} ]]; do
+    sudo dd if=/dev/urandom of=${DEV} bs=4k count=20000 oflag=direct
+    CURRENT_TIME=$(date +%s)
+  done
+}
+
+rbd mirror pool enable ${POOL} image
+rbd mirror pool peer add ${POOL} dummy
+
+# Create images and schedule their mirror snapshots
+for ((i = 1; i <= ${NUM_IMAGES}; i++)); do
+    rbd create --size 1G ${POOL}/${IMAGE_PREFIX}$i
+    rbd mirror image enable ${POOL}/${IMAGE_PREFIX}$i snapshot
+    rbd mirror snapshot schedule add -p ${POOL} --image ${IMAGE_PREFIX}$i 1m
+done
+
+# Run dd workloads on images via kernel client
+for ((i = 1; i <= ${NUM_IMAGES}; i++)); do
+    DEVS[$i]=$(sudo rbd device map ${POOL}/${IMAGE_PREFIX}$i)
+    per_image_workload ${DEVS[$i]} &> /dev/null &
+    DD_PIDS="$DD_PIDS $!"
+done
+
+function cleanup() {
+    set +e
+
+    sudo killall -e dd || true
+    wait
+    for (( i = 1; i <= ${NUM_IMAGES}; i++)); do
+        sudo rbd device unmap ${DEVS[$i]}
+    done
+}
+
+trap cleanup INT TERM EXIT
+
+# Repeatedly blocklist rbd_support module's client ~10s after the module
+# recovers from previous blocklisting
+CURRENT_TIME=$(date +%s)
+END_TIME=$((${CURRENT_TIME}+${RUN_TIME}))
+PREV_CLIENT=""
+CLIENT_ADDR=""
+while [[ ${CURRENT_TIME} -le ${END_TIME} ]]; do
+    if [[ ! -z "${CLIENT_ADDR}" ]] &&
+       [[ "${CLIENT_ADDR}" != "${PREV_CLIENT}" ]]; then
+            ceph osd blocklist add ${CLIENT_ADDR}
+            # Confirm rbd_support module's client is blocklisted
+            ceph osd blocklist ls | grep -q ${CLIENT_ADDR}
+            PREV_CLIENT=${CLIENT_ADDR}
+    fi
+    sleep 10
+    CLIENT_ADDR=$(ceph mgr dump |
+        jq .active_clients[] |
+        jq 'select(.name == "rbd_support")' |
+        jq -r '[.addrvec[0].addr, "/", .addrvec[0].nonce|tostring] | add')
+    CURRENT_TIME=$(date +%s)
+done
+
+# Confirm that rbd_support module recovered from repeated blocklisting
+# Check that you can add a mirror snapshot schedule after a few retries
+for ((i = 1; i <= 24; i++)); do
+    rbd mirror snapshot schedule add -p ${POOL} \
+        --image ${IMAGE_PREFIX}1 2m && break
+    sleep 10
+done
+rbd mirror snapshot schedule ls -p ${POOL} --image ${IMAGE_PREFIX}1 |
+    grep 'every 2m'
+# Verify that the schedule present before client blocklisting is preserved
+rbd mirror snapshot schedule ls -p ${POOL} --image ${IMAGE_PREFIX}1 |
+    grep 'every 1m'
+rbd mirror snapshot schedule rm -p ${POOL} --image ${IMAGE_PREFIX}1 2m
+for ((i = 1; i <= ${NUM_IMAGES}; i++)); do
+    rbd mirror snapshot schedule rm -p ${POOL} --image ${IMAGE_PREFIX}$i 1m
+done
+
+echo OK

746df4fc2529c11dd48eaec277c9e2e28c9cde7d
Ramana Raja <rraja@redhat.com>
2023-12-04 14:14:27 -0500

    qa/workunits/rbd/mirror_compare.sh: line wrap and other cleanup
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- qa/workunits/rbd/mirror_compare.sh
+++ qa/workunits/rbd/mirror_compare.sh
@@ -26,7 +26,8 @@ run_bench() {
 
   KERNEL_TAR_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.280.tar.gz"
   sudo wget $KERNEL_TAR_URL -O $mountpt/kernel.tar.gz
-  sudo timeout $timeout bash -c "tar xvfz $mountpt/kernel.tar.gz -C $mountpt | pv -L 1k --timer &> /dev/null" || true
+  sudo timeout $timeout bash -c "tar xvfz $mountpt/kernel.tar.gz -C $mountpt |\
+    pv -L 1k --timer &> /dev/null" || true
 }
 
 wait_for_demote_snap () {
@@ -35,7 +36,8 @@ wait_for_demote_snap () {
   local image=$3
 
   for s in 1 2 4 8 8 8 8 8 8 8 8 16 16; do
-    RET=`rbd --cluster $cluster snap ls --all $pool/$image | tail -n 1 |grep non_primary | grep demote | grep -v "%" ||true`
+    RET=$(rbd --cluster $cluster snap ls --all $pool/$image | tail -n 1 |
+          grep non_primary | grep demote | grep -v "%" || true)
     if [ "$RET" != "" ]; then
       echo demoted snapshot received, continuing
       sleep 10s #wait a bit for it to propagate
@@ -50,17 +52,18 @@ wait_for_demote_snap () {
 compare_images() {
     local j=$1
 
-    sudo umount ${MOUNT}${j}
-    unmap ${CLUSTER1} ${POOL} ${IMAGE}${j}
     demote_image ${CLUSTER1} ${POOL} ${IMAGE}${j}
 
-    DEMOTE=$(rbd --cluster ${CLUSTER1} snap ls --all ${POOL}/${IMAGE}${j} | tail -n 1 | grep mirror\.primary | grep demoted)
+    DEMOTE=$(rbd --cluster ${CLUSTER1} snap ls --all ${POOL}/${IMAGE}${j} |
+             tail -n 1 | grep mirror\.primary | grep demoted)
     if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
       DEMOTE_ID=$(echo $DEMOTE | awk '{print $1}')
-      BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} --snap-id ${DEMOTE_ID} ${POOL}/${IMAGE}${j})
+      BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE}
+             --snap-id ${DEMOTE_ID} ${POOL}/${IMAGE}${j})
     elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
       DEMOTE_NAME=$(echo $DEMOTE | awk '{print $2}')
-      BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} ${POOL}/${IMAGE}${j}@${DEMOTE_NAME})
+      BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE}
+             ${POOL}/${IMAGE}${j}@${DEMOTE_NAME})
     else
        echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
        return 1
@@ -71,13 +74,16 @@ compare_images() {
 
     promote_image ${CLUSTER2} ${POOL} ${IMAGE}${j}
 
-    PROMOTE=$(rbd --cluster ${CLUSTER2} snap ls --all ${POOL}/${IMAGE}${j} | tail -n 1 | grep mirror\.primary)
+    PROMOTE=$(rbd --cluster ${CLUSTER2} snap ls --all ${POOL}/${IMAGE}${j} |
+              tail -n 1 | grep mirror\.primary)
     if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
       PROMOTE_ID=$(echo $PROMOTE | awk '{print $1}')
-      BDEV=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE} --snap-id ${PROMOTE_ID} ${POOL}/${IMAGE}${j})
+      BDEV=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE}
+             --snap-id ${PROMOTE_ID} ${POOL}/${IMAGE}${j})
     elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
       PROMOTE_NAME=$(echo $PROMOTE | awk '{print $2}')
-      BDEV=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE} ${POOL}/${IMAGE}${j}@${PROMOTE_NAME})
+      BDEV=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE}
+             ${POOL}/${IMAGE}${j}@${PROMOTE_NAME})
     else
        echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
        return 1
@@ -99,9 +105,10 @@ for i in {1..10};
 do
   for j in {1..10};
   do
-    create_image_and_enable_mirror ${CLUSTER1} ${POOL} ${IMAGE}${j} ${RBD_MIRROR_MODE} 10G
-
-    BDEV=$(map ${CLUSTER1} ${POOL} ${IMAGE}${j})
+    create_image_and_enable_mirror ${CLUSTER1} ${POOL} ${IMAGE}${j} \
+      ${RBD_MIRROR_MODE} 10G
+    BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE}
+           ${POOL}/${IMAGE}${j})
     sudo mkfs.ext4 ${BDEV}
     sudo mkdir -p ${MOUNT}${j}
     sudo mount ${BDEV} ${MOUNT}${j}
@@ -113,6 +120,8 @@ do
   pids=''
   for j in {1..10};
   do
+    sudo umount ${MOUNT}${j}
+    sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} ${BDEV}
     compare_images $j &
     pids+=" $!"
   done

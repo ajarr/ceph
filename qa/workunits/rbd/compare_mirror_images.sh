ac45020e38b9ebc4aeea3e4992b22407a9475e1f
Ramana Raja <rraja@redhat.com>
2024-01-29 17:14:18 -0500

    compare_mirror_image_alternate_primary: Ilya's comments
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- qa/workunits/rbd/compare_mirror_images.sh
+++ qa/workunits/rbd/compare_mirror_images.sh
@@ -2,14 +2,20 @@
 
 set -ex
 
-IMG_PREFIX=image-
+CEPH_ARGS=''
+IMG_PREFIX=image-primary
+MIRROR_IMAGE_MODE=snapshot
+MIRROR_POOL_MODE=image
+MNTPT_PREFIX=test-primary
+RBD_IMAGE_FEATURES='layering,exclusive-lock,object-map,fast-diff'
+RBD_MIRROR_INSTANCES=1
 RBD_MIRROR_MODE=snapshot
-MNTPT_PREFIX=/mnt/test
+RBD_MIRROR_USE_EXISTING_CLUSTER=1
 WORKLOAD_TIMEOUT=5m
 
 . $(dirname $0)/rbd_mirror_helpers.sh
 
-launch_manual_msnaps() {
+take_mirror_snapshots() {
   local cluster=$1
   local pool=$2
   local image=$3
@@ -20,29 +26,29 @@ launch_manual_msnaps() {
   done
 }
 
-run_bench() {
-  local mountpt=$1
-  local timeout=$2
+slow_untar_workload() {
+  local tarball_src_path=$1
+  local mountpt=$2
+  local timeout=$3
 
-  KERNEL_TAR_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.280.tar.gz"
-  sudo wget $KERNEL_TAR_URL -O $mountpt/kernel.tar.gz
-  sudo timeout $timeout bash -c "tar xvfz $mountpt/kernel.tar.gz -C $mountpt \
-    | pv -L 1k --timer &> /dev/null" || true
+  cp $tarball_src_path $mountpt/kernel.tar.gz
+  timeout $timeout bash -c "tar xvfz $mountpt/kernel.tar.gz -C $mountpt \
+    | pv -L 1k --quiet > /dev/null" || true
 }
 
 wait_for_non_primary_demoted_mirror_snap() {
   local cluster=$1
   local pool=$2
   local image=$3
-  local ret
+  local demoted=false
 
   for s in 1 2 4 8 8 8 8 8 8 8 8 16 16; do
-    ret=$(rbd --cluster $cluster snap ls --all $pool/$image --format=json \
-            | jq 'last' \
-            | jq 'select(.name | contains("non_primary"))' \
-            | jq 'select(.namespace.state == "demoted")' \
-            | jq 'select(.namespace.complete == true)')
-    if [ "$ret" != "" ]; then
+    demoted=$(rbd --cluster $cluster snap ls --all $pool/$image --format=json \
+              | jq '(last | .name | startswith(".mirror.non_primary")) and
+                    (last | .namespace.state == "demoted") and
+                    (last | .namespace.complete == true)')
+
+    if [ "$demoted" = true ]; then
       echo "demoted snapshot received, continuing"
       return 0
     fi
@@ -60,12 +66,9 @@ wait_for_image_removal () {
   local cluster=$1
   local pool=$2
   local image=$3
-  local img_in_list
 
   for s in 1 2 4 8 8 8 8 8 8 8 8 16 16; do
-    img_in_list=$(rbd --cluster $cluster ls -p $pool --format=json \
-                    | jq --arg img $image 'index($img)')
-    if [[ $img_in_list == null ]]; then
+    if [[ -z $(rbd --cluster $cluster ls $pool | grep -w $image) ]]; then
       echo "image:${image} removed from cluster:${cluster} pool:${pool}"
       return 0
     fi
@@ -80,57 +83,37 @@ wait_for_image_removal () {
 }
 
 compare_demoted_promoted_mirror_snaps() {
-  local img=${IMG_PREFIX}$1
-  local mntpt=${MNTPT_PREFIX}$1
-  local bdev demote demote_id demote_name demote_md5
-  local promote promote_id promote_name promote_md5
+  local dev=$1
+  local img=${IMG_PREFIX}$2
+  local mntpt=${MNTPT_PREFIX}$2
+  local demote_md5 promote_md5
 
   sudo umount ${mntpt}
+
+  # calculate hash before demotion of primary image
+  demote_md5=$(sudo md5sum ${dev} | awk '{print $1}')
   sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} \
       ${POOL}/${img}
 
-  # demote primary image and calculate hash of its latest mirror snapshot
   demote_image ${CLUSTER1} ${POOL} ${img}
-  demote=$(rbd --cluster ${CLUSTER1} snap ls --all ${POOL}/${img} \
-             --format=json \
-             | jq 'last' \
-             | jq 'select(.name | contains("mirror.primary"))' \
-             | jq 'select(.namespace.state == "demoted")')
-  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
-    demote_id=$(echo $demote | jq -r '.id')
-    bdev=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
-             -o try-netlink --snap-id ${demote_id} ${POOL}/${img})
-  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
-    demote_name=$(echo $demote | jq -r '.name')
-    bdev=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
-             ${POOL}/${img}@${demote_name})
-  fi
-  demote_md5=$(sudo md5sum ${bdev} | awk '{print $1}')
-  sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} ${bdev}
-
   wait_for_non_primary_demoted_mirror_snap ${CLUSTER2} ${POOL} ${img}
-
-  # promote non-primary image and calculate hash of its latest mirror snapshot
+  sleep 10
+  # wait_for_status_in_pool_dir ${CLUSTER1} ${POOL} ${img} 'up+unknown'
+  # wait_for_status_in_pool_dir ${CLUSTER2} ${POOL} ${img} 'up+unknown'
   promote_image ${CLUSTER2} ${POOL} ${img}
-  promote=$(rbd --cluster ${CLUSTER2} snap ls --all ${POOL}/${img} \
-              --format=json \
-              | jq 'last' \
-              | jq 'select(.name | contains("mirror.primary"))')
-  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
-    promote_id=$(echo $promote | jq -r '.id')
-    bdev=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE} \
-             -o try-netlink --snap-id ${promote_id} ${POOL}/${img})
-  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
-    promote_name=$(echo $promote | jq -r '.name')
-    bdev=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE} \
-             ${POOL}/${img}@${promote_name})
-  fi
-  promote_md5=$(sudo md5sum ${bdev} | awk '{print $1}')
-  sudo rbd --cluster ${CLUSTER2} device unmap -t ${RBD_DEVICE_TYPE} ${bdev}
 
-  if [ "${demote_md5}" != "${promote_md5}" ]; then
-    echo "demote_md5:${demote_md5} != promote_md5:${promote_md5} for \
-      pool/img:${POOL}/${img}"
+  # calculate hash after promotion of secondary image
+  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+    dev=$(sudo rbd --cluster ${CLUSTER2} device map -t nbd \
+             -o try-netlink ${POOL}/${img})
+  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+    dev=$(sudo rbd --cluster ${CLUSTER2} device map -t krbd ${POOL}/${img})
+  fi
+  promote_md5=$(sudo md5sum ${dev} | awk '{print $1}')
+  sudo rbd --cluster ${CLUSTER2} device unmap -t ${RBD_DEVICE_TYPE} ${dev}
+
+  if [[ "${demote_md5}" != "${promote_md5}" ]]; then
+    echo "Mismatch for ${POOL}/${img}: ${demote_md5} != ${promote_md5}"
     return 1
   fi
 }
@@ -140,34 +123,40 @@ setup
 start_mirrors ${CLUSTER1}
 start_mirrors ${CLUSTER2}
 
+TARBALL_SRC=kernel.tar.gz
+wget https://download.ceph.com/qa/linux-5.4.tar.gz -O ${TARBALL_SRC}
+
 for i in {1..10}; do
+  DEVS=()
   for j in {1..10}; do
     IMG=${IMG_PREFIX}${j}
     MNTPT=${MNTPT_PREFIX}${j}
     create_image_and_enable_mirror ${CLUSTER1} ${POOL} ${IMG} \
       ${RBD_MIRROR_MODE} 10G
     if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
-      BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
-               -o try-netlink ${POOL}/${IMG})
+      DEV=$(sudo rbd --cluster ${CLUSTER1} device map -t nbd \
+	      -o try-netlink ${POOL}/${IMG})
     elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
-      BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
-               ${POOL}/${IMG})
+      DEV=$(sudo rbd --cluster ${CLUSTER1} device map -t krbd \
+	      ${POOL}/${IMG})
     else
       echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
       return 1
     fi
-    sudo mkfs.ext4 ${BDEV}
-    sudo mkdir -p ${MNTPT}
-    sudo mount ${BDEV} ${MNTPT}
-    # create mirror snapshots under I/O
-    launch_manual_msnaps ${CLUSTER1} ${POOL} ${IMG} &
-    run_bench ${MNTPT} ${WORKLOAD_TIMEOUT} &
+    DEVS+=($DEV)
+    sudo mkfs.ext4 ${DEV}
+    mkdir ${MNTPT}
+    sudo mount ${DEV} ${MNTPT}
+    sudo chown $(whoami) ${MNTPT}
+    # create mirror snapshots under I/O every few seconds
+    take_mirror_snapshots ${CLUSTER1} ${POOL} ${IMG} &
+    slow_untar_workload ${TARBALL_SRC} ${MNTPT} ${WORKLOAD_TIMEOUT} &
   done
   wait
 
   pids=''
   for j in {1..10}; do
-    compare_demoted_promoted_mirror_snaps $j &
+    compare_demoted_promoted_mirror_snaps ${DEVS[$j-1]} $j &
     pids+=" $!"
   done
 
@@ -187,6 +176,7 @@ for i in {1..10}; do
     wait_for_replaying_status_in_pool_dir ${CLUSTER1} ${POOL} ${IMG}
     remove_image ${CLUSTER2} ${POOL} ${IMG}
     wait_for_image_removal ${CLUSTER1} ${POOL} ${IMG}
+    rm -rf ${MNTPT_PREFIX}${j}
   done
 done
 
